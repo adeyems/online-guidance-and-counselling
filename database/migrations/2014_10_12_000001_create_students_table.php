f6a850f2f99f09ecd85fbb6970c96f076359d61c
donhalogen <donhalogen@gmail.com>
2019-08-18 10:03:17 +0100

    update

--- database/migrations/2014_10_12_000001_create_students_table.php
+++ database/migrations/2014_10_12_000001_create_students_table.php
@@ -26,7 +26,8 @@ class CreateStudentsTable extends Migration
             $table->string('email')->unique();
             $table->string('class_teacher_name');
             $table->string('class_teacher_surname');
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('verified')->default('no');
+            $table->string('verification_token')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
